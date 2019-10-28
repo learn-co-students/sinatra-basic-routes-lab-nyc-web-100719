23dac164140d37d05c917c3c8794ce98031dcec3
Bailey Wood <woodbmae@gmail.com>
2019-10-28 14:03:06 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        200
+        "My name is Bailey"
+    end
+
+    get '/hometown' do
+        200
+        "My hometown is Milwaukee"
+    end
+
+    get '/favorite-song' do
+        200
+        "My favorite song is black bird"
+    end
+
 end
